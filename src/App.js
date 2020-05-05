b698a462a4215d94f52930798afd62174bd1f5b9
sush <sushmitharangam@gmail.com>
2020-05-05 22:43:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+ 
+  constructor(props){
+    
+    super(props);
+
+    this.state = {
+      newItem: '',
+      list: []
+    }
+  }
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem(){
+    const newItem = { 
+      id : 1 + Math.random(),
+      value: this.state.newItem.slice()      
+    };
+  
+  const list = this.state.list;  
+  list.push(newItem);
+  // this.state.list.push(newItem)
+   
+    this.setState({
+      list, 
+      newItem:""
+    });
+  }
+  componentDidMount(){
+    this.addItem();
+    console.log('Component did mount')
+  }
+  deleteItem(id){
+    const list =[...this.state.list];
+    const updatedList = list.filter(
+      item => item.id!== id
+    )
+
+    this.setState({
+      list: updatedList
+    });
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        My To-do List<br/>
+        <br/>
+        <input 
+          type= 'text' 
+          placeholder = 'Type the To-do list items here'
+          value = {this.state.newItem}
+            onChange={
+              event => this.updateInput('newItem',event.target.value)
+            }  
+        />
+        <button 
+          onClick= {()=> this.addItem()}>
+            Add Item to the List
+        </button>
+      <br/>
+      <ul>
+          {this.state.list.map(item=>{
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button 
+                    onClick ={() => this.deleteItem(item.id)}> Delete  
+                  </button>
+                </li>
+              )
+            }
+          )}
+      </ul>
+      </div>
     </div>
   );
 }
-
+}
 export default App;
